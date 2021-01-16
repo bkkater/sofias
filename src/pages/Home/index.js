7e06ee4faff89c2b299c690b998d67e6b7ffbf8e
Bianca <biancamedeiros.bs10@gmail.com>
2021-01-16 15:59:43 -0300

    feat: Página Home Criada

---
+++ src/pages/Home/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './styles.scss';
+
+function Home() {
+  return (
+    <div className="page-home">
+      <h1>Olá</h1>
+    </div>
+  );
+}
+
+export default Home;
