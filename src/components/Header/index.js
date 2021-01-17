03091e9c257e62cf22ee191f31e612a7e714c60a
Bianca <biancamedeiros.bs10@gmail.com>
2021-01-17 00:17:17 -0300

    feat: Adicionando a logo do projeto no Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,14 +1,19 @@
-import React from "react";
+import React from 'react';
 
-import { FiMenu } from "react-icons/fi";
-import { Link } from "react-router-dom";
+import { FiMenu } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 
-import "./styles.scss";
+import logo from '~/resources/assets/logo.png';
+
+import './styles.scss';
 
 function Header() {
   return (
     <div className="d-flex justify-content-between align-items-center mb-5">
-      <FiMenu color="black" size={24} />
+      <div>
+        <FiMenu color="black" size={24} />
+        <img src={logo} alt="Sofias" className="ml-3" />
+      </div>
 
       <span>
         Já tem conta? <Link>Acessar</Link>
