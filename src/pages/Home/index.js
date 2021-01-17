7b9b91d30902f72d626a8548b5ab09c71a35a6ae
Bianca <biancamedeiros.bs10@gmail.com>
2021-01-17 09:25:10 -0300

    update: Modificação do Layout da Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -14,27 +14,31 @@ import LocationCard from '~/components/UI/LocationCard';
 function Home() {
   return (
     <Page title="Escolha uma área">
-      <Search />
+      <div className="d-flex flex-column justify-content-between flex-fill">
+        <div>
+          <Search />
 
-      <div className="d-flex mt-3 mb-3 card__list">
-        {cardList.map((cardList) => (
+          <div className="d-flex mt-3 mb-3 card__list">
+            {cardList.map((cardList) => (
+              <Card
+                label={cardList.label}
+                key={cardList.id}
+                image={cardList.image}
+              />
+            ))}
+          </div>
+
           <Card
-            label={cardList.label}
-            key={cardList.id}
-            image={cardList.image}
+            label="Em dúvida?"
+            description="Faça um teste"
+            image={cardImage}
+            fullWidth
           />
-        ))}
-      </div>
+        </div>
 
-      <Card
-        label="Em dúvida?"
-        description="Faça um teste"
-        image={cardImage}
-        fullWidth
-      />
-
-      <div className="d-flex card__location__container mt-5 justify-content-center">
-        <LocationCard />
+        <div className="d-flex card__location__container mb-4 mt-5 justify-content-center">
+          <LocationCard />
+        </div>
       </div>
     </Page>
   );
